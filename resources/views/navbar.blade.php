844fa7b53a8d52cadb46c1e4d789521ef3e3a3e7
akirosefederez <akirosefederez99@gmail.com>
2026-01-23 18:26:05 +0800

    Fixed Navbar Scrolling

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,87 +1,64 @@
-        <!-- Navbar --> 
- <div class="image-wrapper">
-    <div class="navimage">
-        <img src="{{ asset('./assets/NAVIGATION BAR.png') }}" alt="Metta Image" class="main-image">
-    </div>
-        <div class="overlay-content">
-            <div class="container-fluid">
-                <div class="row justify-content-center align-items-center text-center">
+<!-- HEADER (OVERLAY / RESPONSIVE) -->
+<header
+  class="header-bg"
+  id="siteHeader"
+>
+  <div class="header-content">
+    <div class="container-fluid">
+      <div class="row align-items-center">
 
-                    <!-- Column 1 -->
-                    <div class="col-md-4 mb-2 d-flex justify-content-center">
-                        <div class="overlay-box">
-                            <nav class="pill-navbar desktop-only">
-                                <a href="#">Home</a>
-                                <a href="#">About Us</a>
-                                <a href="#" class="active">Tickets</a>
-                                <a href="#">Contact</a>
-
-                                <div class="dropdown">
-                                    <a class="dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
-                                        Merch
-                                    </a>
-                                    <ul class="dropdown-menu">
-                                        <li><a class="dropdown-item" href="#">Shirts</a></li>
-                                        <li><a class="dropdown-item" href="#">Hoodies</a></li>
-                                        <li><a class="dropdown-item" href="#">Accessories</a></li>
-                                    </ul>
-                                </div>
-                            </nav>
-                            <div class="dropdown pill-navbar-dropdown mobile-only">
-                                <a class="dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    &#9776;
-                                </a>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="#">Home</a></li>
-                                    <li><a class="dropdown-item" href="#">About Us</a></li>
-                                    <li><a class="dropdown-item" href="#">Tickets</a></li>
-                                    <li><a class="dropdown-item" href="#">Contact</a></li>
-                                    <li><hr class="dropdown-divider"></li>
-                                    <li><a class="dropdown-item" href="#">Merch</a></li>
-                                    <li><a class="dropdown-item" href="#">Shirts</a></li>
-                                    <li><a class="dropdown-item" href="#">Hoodies</a></li>
-                                    <li><a class="dropdown-item" href="#">Accessories</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-
-                    <!-- Column 2 -->
-                    <div class="col-md-4 mb-2 d-flex justify-content-center">
-                        <div class="overlay-box">
-                            <img src="{{ asset('./assets/METTACITY LOGO.png') }}" alt="METTACITY Logo" class="logo-img">
-                            <img src="{{ asset('./assets/MEEKO HEAD EMBLEM.png') }}" alt="METTACITY MEEKO Logo" class="meekologo-img">
-                        </div>
-                    </div>
-
-                    <!-- Column 3 -->
-                    <div class="col-md-4 mb-2 d-flex justify-content-center">
-                        <div class="overlay-box">
-                            <div class="visit-social">
-
-                                <!-- Plan Your Visit -->
-                                <a href="https://example.com/visit">
-                                    <img src="{{ asset('./assets/PLAN YOUR VISIT.png') }}" alt="Plan Your Visit">
-                                </a>
-
-                                <!-- Social Icons -->
-                                <div class="social-icons">
-                                    <a href="https://www.facebook.com/MettaCityPH" target="_blank">
-                                        <img src="{{ asset('./assets/FB.png') }}" alt="Facebook">
-                                    </a>
-                                    <a href="https://youtube.com" target="_blank">
-                                        <img src="{{ asset('./assets/YT.png') }}" alt="YouTube">
-                                    </a>
-                                    <a href="https://instagram.com" target="_blank">
-                                        <img src="{{ asset('./assets/IG.png') }}" alt="Instagram">
-                                    </a>
-                                </div>
-
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
+        <!-- MOBILE: HAMBURGER ONLY -->
+        <div class="col-12 d-flex align-items-center justify-content-start d-md-none">
+          <button class="menu-btn" type="button"
+                  data-bs-toggle="offcanvas"
+                  data-bs-target="#mobileMenu"
+                  aria-controls="mobileMenu"
+                  aria-label="Open menu">
+            <i class="fa-solid fa-bars"></i>
+          </button>
+          <div class="mobile-header-socials ms-auto">
+            <a href="#" class="mobile-header-social"><i class="fa-brands fa-facebook-f"></i></a>
+            <a href="#" class="mobile-header-social"><i class="fa-brands fa-instagram"></i></a>
+            <a href="#" class="mobile-header-social"><i class="fa-brands fa-x-twitter"></i></a>
+          </div>
         </div>
+
+        <!-- DESKTOP: LEFT NAV -->
+        <div class="col-4 d-none d-md-flex align-items-center">
+          <ul class="header-nav">
+            <li><a class="nav-link" href="#">Home</a></li>
+            <li><a class="nav-link" href="#">About Us</a></li>
+            <li><a class="nav-link active" href="../II_ENTER METTACITY/III_TICKETING/ticketing.html"">Tickets</a></li>
+            <li><a class="nav-link" href="#">Contact</a></li>
+
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">Merch</a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Shirts</a></li>
+                <li><a class="dropdown-item" href="#">Hats</a></li>
+                <li><a class="dropdown-item" href="#">Accessories</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+
+        <!-- DESKTOP: LOGO -->
+        <div class="col-4 col-mid d-none d-md-flex">
+          <img src="{{ asset('./assets/METTACITY Logo.png') }}" class="logo-img" alt="Mettacity Logo">
+        </div>
+
+        <!-- DESKTOP: CTA + SOCIAL -->
+        <div class="col-4 d-none d-md-flex align-items-center justify-content-end gap-3 pe-4">
+          <img src="{{ asset('./assets/PLAN YOUR VISIT.png') }}" class="plan-visit-img" alt="Plan Your Visit">
+
+          <a href="#" class="social-icon"><i class="fa-brands fa-facebook-f"></i></a>
+          <a href="#" class="social-icon"><i class="fa-brands fa-instagram"></i></a>
+          <a href="#" class="social-icon"><i class="fa-brands fa-x-twitter"></i></a>
+        </div>
+        
+      </div>
     </div>
+  </div>
+  <img src="{{ asset('./assets/NAVIGATIONBAR.png') }}" alt="Navigation Bar Background" class="header-bg-img">
+
+</header>
